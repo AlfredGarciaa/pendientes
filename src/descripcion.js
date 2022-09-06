e9b7d7922a8a063d8613fc5da209f2a979098844
Alfred <alfred.garcia@ucb.edu.bo>
2022-09-06 10:07:35 -0400

    Caracteres maximos de 50 en descripcion ... INCOMPLETO

--- src/descripcion.js
+++ src/descripcion.js
@@ -1,7 +1,16 @@
 function añadir(descripcion) {
-  
   alert("No es posible registrar una tarea pendiente sin descripción.");
 
+  /*for(i=0; i<fields.length; i++){
+    document.getElementById(fields[i]).maxLength = 50;
+  }
+  var maximo = 50;
+  if (descripcion > maximo) {
+    return descripcion;
+  }
+  else{
+    alert("Logitud maxima sobrepasa !!!");
+  }*/
   return descripcion;
 }
 
